b74a3e6396870e435f4f61e34612cfd5abebfa83
vladk7072 <vladk7072@gmail.com>
2023-02-26 23:17:02 +0200

    feat: index, distanse layout (no adaptive)

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+  host: "185.137.235.119", // адрес FTP сервера
+  user: "kravche1", // имя пользователя
+  password: "znp48Ge86Y", // пароль
+  parallel: 5 // кол-во одновременных потоков
+}
---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+const header = document.querySelector(".header");
+
+window.onscroll = function () {
+  if (window.pageYOffset >= 100) {
+    header.classList.add("header--scroll");
+  }
+  if (window.pageYOffset < 100) {
+    header.classList.remove("header--scroll");
+  }
+};
+
+const swiperGallery = new Swiper(".gallery__slider", {
+  centeredSlides: true,
+  slidesPerView: 2,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: ".gallery__top-btn-next",
+    prevEl: ".gallery__top-btn-prev",
+  },
+});
+const swiperReviews = new Swiper(".reviews__slider", {
+  slidesPerView: 2.14,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".reviews__top-btn-next",
+    prevEl: ".reviews__top-btn-prev",
+  },
+});
